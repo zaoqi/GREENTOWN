5d77d1d8bcc179e495bd735b6057d939bd8ab7e0
zaoqi <zaomir@outlook.com>
2018-07-28 21:56:16 +0800

    .

---
+++ 2018/7/homework/Volcano.ts
@@ -0,0 +1,27 @@
+<TeXmacs|1.99.5>
+
+<style|<tuple|generic|chinese>>
+
+<\body>
+  <doc-data|<doc-title|<label|result_box>Volcano: the origin of
+  life>|<doc-author|<author-data|<author-name|zaoqi>|<author-email|zaomir@outlook.com>>>>
+
+  <paragraph|><label|result_box>The origin of life requires energy, specific
+  substances,which are provided by volcanoes.
+</body>
+
+<\references>
+  <\collection>
+    <associate|auto-1|<tuple|1|?|../../../home/zaoqi/.TeXmacs/texts/scratch/no_name_2.tm>>
+    <associate|result_box|<tuple|1|?|../../../home/zaoqi/.TeXmacs/texts/scratch/no_name_2.tm>>
+  </collection>
+</references>
+
+<\auxiliary>
+  <\collection>
+    <\associate|toc>
+      <with|par-left|<quote|4tab>| <datoms|<macro|x|<repeat|<arg|x>|<with|font-series|medium|<with|font-size|1|<space|0.2fn>.<space|0.2fn>>>>>|<htab|5mm>>
+      <no-break><pageref|auto-1><vspace|0.15fn>>
+    </associate>
+  </collection>
+</auxiliary>
